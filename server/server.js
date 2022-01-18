7f44d6ad53089fba5a5da4141a05c202b4f6709c
Nikola Kelava <nikola.kelava94@gmail.com>
2022-01-18 19:53:27 +0100

    Update files

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(cors());
 
 // app.get("/", (req, resp) => { resp.send(database.select("*").from("users")) });
-app.get("/", (req, resp) => { resp.send("Hello!");
+app.get("/", (req, resp) => { resp.send("Hello!") });
 
 app.post("/signin", (req, resp) => { signin.handleSignIn(req, resp, database, bcrypt) });
 
