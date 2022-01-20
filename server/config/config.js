5b15fc1e3e33b5052be29c57ccc1f473b833d0c7
Nikola Kelava <nikola.kelava94@gmail.com>
2022-01-20 17:46:29 +0100

    Modify clarifai var

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,6 @@ dotenv.config();
 module.exports = {
   NODE_ENV:     process.env.NODE_ENV,
   PORT:         process.env.PORT,
-  CLARIFAI_KEY: process.env.CLARIFAI_API_KEY,
+  CLARIFAI_KEY: process.env.CLARIFAI_KEY,
   DATABASE_URL: process.env.DATABASE_URL
 };
