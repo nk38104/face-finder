87acec5873c9651bccce4d0a9a0b344474fb6e2a
Nikola Kelava <nikola.kelava94@gmail.com>
2022-05-18 16:41:44 +0200

    Ref: remove redundant singin and register links from singin page

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -8,14 +8,8 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
                 <p onClick={() => onRouteChange("signin")} className="f3 b link dim black underline pa3 pointer">Sign Out</p>
             </nav>
         );
-    } else {
-        return (
-            <nav className="flex justify-end">
-                <p onClick={() => onRouteChange("signin")} className="f3 b link dim black underline pa3 pointer">Sign In</p>
-                <p onClick={() => onRouteChange("register")} className="f3 b link dim black underline pa3 pointer">Register</p>
-            </nav>
-        );
     }
+    return (<></>)
 }
 
 export default Navigation;
